6d424d2f375f855b0979496d897ccf8696a48523
unknown <syunoncasual@gmail.com>
2022-07-07 15:15:12 -0400

    added the script of schema initialization

---
+++ ReinburExpense Reimbursement Systemn/Script-2.sql
@@ -0,0 +1,28 @@
+--drop schema ERS_P1;
+--drop table ERS_P1.users;
+--drop table ERS_P1.tickets;
+
+create schema ERS_P1;
+
+create table ERS_P1.users(
+	id int identity,
+	username varchar(100) not null,
+	password varchar(100) not null,
+	role VARCHAR(10) NOT NULL CHECK (role IN('Manager', 'Employee')),
+	primary key(id)
+);
+
+create table ERS_P1.tickets(
+	id int identity,
+	reason varchar(100) not null,
+	status VARCHAR(10) NOT NULL CHECK (status IN('Pending', 'Approved', 'Denied')),
+	authorID int foreign key references ERS_P1.users(id) not null,
+	resolverID int foreign key references ERS_P1.users(id) not null,
+	amount DECIMAL not null,
+	primary key(id)
+);
+
+INSERT into ERS_P1.users (username, password, role) values ('sampleManager', 'ManagerPass', 'Manager');
+INSERT into ERS_P1.users (username, password, role) values ('sampleEmployee', 'EmployeePass', 'Employee');
+
+select * from ERS_P1.users; 
