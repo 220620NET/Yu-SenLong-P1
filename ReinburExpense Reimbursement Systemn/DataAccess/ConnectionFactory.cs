a37379610245c72ceb3767019b4a0f2fb0f9e7cf
unknown <syunoncasual@gmail.com>
2022-07-11 14:30:35 -0400

    updated gitignor for the connectionfactory class

--- ReinburExpense Reimbursement Systemn/DataAccess/ConnectionFactory.cs
+++ ReinburExpense Reimbursement Systemn/DataAccess/ConnectionFactory.cs
@@ -1,6 +1,9 @@
 namespace DataAccess;
+using System.Data.SqlClient;
 
-public class ConnectionFactory
+public static class ConnectionFactory
 {
     
+    private string connectionString = "Server=tcp:syuserver.database.windows.net,1433;Initial Catalog=SenLong's DataBase;Persist Security Info=False;User ID=sqluser;Password=p4ssw0rd!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
 }
